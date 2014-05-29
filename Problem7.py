c7e06d24cfe658d87e7e217fef2cb69899d79991
a-harper <alex@itsec.im>
2014-05-29 12:44:22 +0100

    Problem 7 solved

---
+++ Problem7.py
@@ -0,0 +1,12 @@
+__author__ = 'harpera'
+"""What is the 10 001st prime number?"""
+
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * (n/2)
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i/2]:
+            sieve[i*i/2::i] = [False] * ((n-i*i-1)/(2*i)+1)
+    return [2] + [2*i+1 for i in xrange(1,n/2) if sieve[i]]
+
+print primes(200010)[10000]
