f096911ea40195a3c78d8e3bc2a883d745a92b2f
a-harper <alex@itsec.im>
2014-05-30 15:03:10 +0100

    Problems 19 and 20 Solved

--- Problem20.py
+++ Problem20.py
@@ -1,8 +1,7 @@
 __author__ = 'harpera'
 """Find the sum of the digits in the number 100!"""
 
-def factorial(n):
-    return reduce(lambda x,y: x*y, range(1,n+1,1))
+def factorial(n):  return reduce(lambda x,y: x*y, range(n,0,-1))
 
 def sum_digits(n):
     return sum([int(i) for i in str(n)])
