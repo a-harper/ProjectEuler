11745cf88a199dc79cc8284b15fea2b9e958654f
a-harper <alex@itsec.im>
2014-05-30 15:11:59 +0100

    Problems 19 and 20 Solved

--- Problem20.py
+++ Problem20.py
@@ -3,7 +3,6 @@ __author__ = 'harpera'
 
 def factorial(n):  return reduce(lambda x,y: x*y, range(n,0,-1))
 
-def sum_digits(n):
-    return sum([int(i) for i in str(n)])
+def sum_digits(n):  return sum([int(i) for i in str(n)])
 
 print sum_digits(factorial(100))
